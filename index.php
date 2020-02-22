7dd27394e5430144c76d238dd7813fe8303fde25
szpsuccesfull <61071173+szpsuccesfull@users.noreply.github.com>
2020-02-21 21:21:02 -0500

    hasta el primer ejercicio

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-echo("hola");
+
+// ejercicio numero 1 
+
+echo("taller numero 1 elaborado por samuel zapata");
+print("<br>");
+
+$n1=10;
+$n2=20;
+$suma=$n1+$n2;
+$resta=$n1-$n2;
+$multiplicacion=$n1*$n2;
+$division=$n1/$n2;
+echo ("el resultado de la suma entre n1 y n2 :".$suma);
+
+
+
 
 
 
